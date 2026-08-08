25a3e63df5a57c97dd8de7c468b5b2953918bf9a
andrey_david <andrey.david.qwerty@gmail.com>
2026-08-08 22:56:22 +0300

    refactor: update __init__ imports

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -4,6 +4,7 @@ from .keyboards import (
     regions_keyboard,
     currency_keyboard,
     languages_keyboard,
+    languages_keyboard_on_start,
 )
 from .menu_commands import (
     set_main_menu,
