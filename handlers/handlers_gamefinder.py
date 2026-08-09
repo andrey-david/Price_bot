1ab20bc58b2de06b4bc6e640efcd75d87845b8b6
andrey_david <andrey.david.qwerty@gmail.com>
2026-08-10 01:41:47 +0300

    refactor: restructure game search handler

--- handlers/handlers_gamefinder.py
+++ handlers/handlers_gamefinder.py
@@ -7,30 +7,29 @@ from aiogram.types import (
     InlineQueryResultArticle,
     InputTextMessageContent,
 )
+from aiogram.exceptions import TelegramBadRequest
 
 from get_prices import (
     get_prices,
     format_prices,
 )
-from lexicon import (
-    LEXICON,
-)
-from keyboards import (
-    find_game_keyboard,
-)
-from database import (
-    User,
-)
+from lexicon import LEXICON
+from keyboards import find_game_keyboard
+from database import User
+
 
 logger = logging.getLogger(__name__)
-handlers_gamefider = Router(name='handlers_gamefider')
+
+handlers_gamefider = Router(
+    name="handlers_gamefider"
+)
 
 
 def load_games(filename="games_list.txt"):
     with open(
-            filename,
-            "r",
-            encoding="utf-8"
+        filename,
+        "r",
+        encoding="utf-8"
     ) as file:
         return [
             line.strip()
@@ -53,31 +52,51 @@ async def search_inline(
         await query.answer([])
         return
 
-    results = []
+    games = [
+        game
+        for game in games_list
+        if text in game.lower()
+    ]
 
-    for index, game in enumerate(games_list):
-        if text in game.lower():
-            results.append(
-                InlineQueryResultArticle(
-                    id=str(index),
-                    title=game,
-                    description=LEXICON[user.language]["inline_description"],
-                    input_message_content=InputTextMessageContent(
-                        message_text=game
-                    )
-                )
+    if not games:
+        await query.answer([])
+        return
+
+    results = [
+        InlineQueryResultArticle(
+            id=f"{index}_{game}",
+            title=game,
+            description=LEXICON[user.language][
+                "inline_description"
+            ],
+            input_message_content=InputTextMessageContent(
+                message_text=game
             )
+        )
+        for index, game in enumerate(games[:50])
+    ]
 
-    await query.answer(
-        results[:50],
-        cache_time=0
-    )
+    try:
+        await query.answer(
+            results[:20],
+            cache_time=0,
+            is_personal=True
+        )
+
+    except TelegramBadRequest as e:
+        if "query is too old" in str(e):
+            logger.debug(
+                "Inline query expired: %s",
+                query.id
+            )
+        else:
+            raise
 
 
 @handlers_gamefider.message()
 async def send_result(
-        message: Message,
-        user: User
+    message: Message,
+    user: User
 ):
     msg = await message.reply(
         LEXICON[user.language]["wait"]
@@ -92,27 +111,31 @@ async def send_result(
             )
             return
 
-        user_currency = user.currency
-
         prices = await get_prices(
             message.text,
             regions,
-            user_currency
+            user.currency
         )
 
         await msg.edit_text(
             text=format_prices(
                 prices,
-                user_currency
+                user.currency
             ),
-            reply_markup=find_game_keyboard(language=user.language),
+            reply_markup=find_game_keyboard(
+                language=user.language
+            ),
             disable_web_page_preview=True,
         )
 
     except Exception:
-        logger.exception("Error while getting prices")
+        logger.exception(
+            "Error while getting prices"
+        )
 
         await msg.edit_text(
             LEXICON[user.language]["error"],
-            reply_markup=find_game_keyboard(language=user.language)
+            reply_markup=find_game_keyboard(
+                language=user.language
+            )
         )
