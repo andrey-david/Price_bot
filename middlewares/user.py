43c3eb63d4b081055084bbe5f3aef02bccdb9712
andrey_david <andrey.david.qwerty@gmail.com>
2026-08-14 04:16:05 +0300

    refactor: improve user middleware initialization flow

--- middlewares/user.py
+++ middlewares/user.py
@@ -7,10 +7,10 @@ from database import async_session, User
 
 class UserMiddleware(BaseMiddleware):
     async def __call__(
-            self,
-            handler,
-            event: TelegramObject,
-            data: dict
+        self,
+        handler,
+        event: TelegramObject,
+        data: dict
     ):
         telegram_user = data.get("event_from_user")
 
@@ -19,11 +19,24 @@ class UserMiddleware(BaseMiddleware):
 
         async with async_session() as session:
             user = await session.scalar(
-                select(User)
-                .where(User.telegram_id == telegram_user.id)
+                select(User).where(
+                    User.telegram_id == event.from_user.id
+                )
             )
 
+            if user is None:
+                user = User(
+                    telegram_id=event.from_user.id,
+                    username=event.from_user.username,
+                    first_name=event.from_user.first_name,
+                    language="ru",
+                    currency="USD",
+                )
+
+                session.add(user)
+                await session.commit()
+
             data["user"] = user
-            data["locale"] = user.language if user else "ru"
+            data["language"] = user.language
 
         return await handler(event, data)
