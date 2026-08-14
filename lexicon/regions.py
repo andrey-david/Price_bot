8440452e8668e3f749e13e60809fde4f52494a82
andrey_david <andrey.david.qwerty@gmail.com>
2026-08-15 01:58:36 +0300

    refactor: regroup regions and remove currency codes

--- lexicon/regions.py
+++ lexicon/regions.py
@@ -3,370 +3,301 @@ REGIONS = [
     {
         "ps_locale": "es-ar",
         "country": "🇦🇷 Argentina",
-        "currency": "USD",  # ARS
     },
     {
         "ps_locale": "es-bo",
         "country": "🇧🇴 Bolivia",
-        "currency": "USD",  # BOB
     },
     {
         "ps_locale": "pt-br",
         "country": "🇧🇷 Brazil",
-        "currency": "BRL",
     },
     {
         "ps_locale": "en-ca",
         "country": "🇨🇦 Canada",
-        "currency": "CAD",
     },
     {
         "ps_locale": "es-cl",
         "country": "🇨🇱 Chile",
-        "currency": "USD",  # CLP
     },
     {
         "ps_locale": "es-co",
         "country": "🇨🇴 Colombia",
-        "currency": "USD",  # COP
     },
     {
         "ps_locale": "es-cr",
         "country": "🇨🇷 Costa Rica",
-        "currency": "USD",  # CRC
     },
     {
         "ps_locale": "es-ec",
         "country": "🇪🇨 Ecuador",
-        "currency": "USD",
     },
     {
         "ps_locale": "es-sv",
         "country": "🇸🇻 El Salvador",
-        "currency": "USD",
     },
     {
         "ps_locale": "es-gt",
         "country": "🇬🇹 Guatemala",
-        "currency": "USD",  # GTQ
     },
     {
         "ps_locale": "es-hn",
         "country": "🇭🇳 Honduras",
-        "currency": "USD",  # HNL
     },
     {
         "ps_locale": "es-mx",
         "country": "🇲🇽 Mexico",
-        "currency": "USD",  # TODO: заменить на MXN 20.08.2026
     },
     {
         "ps_locale": "es-ni",
         "country": "🇳🇮 Nicaragua",
-        "currency": "USD",  # NIO
     },
     {
         "ps_locale": "es-pa",
         "country": "🇵🇦 Panama",
-        "currency": "USD",
     },
     {
         "ps_locale": "es-py",
         "country": "🇵🇾 Paraguay",
-        "currency": "USD",  # PYG
     },
     {
         "ps_locale": "es-pe",
         "country": "🇵🇪 Peru",
-        "currency": "USD",  # PEN
     },
     {
         "ps_locale": "en-us",
         "country": "🇺🇸 United States",
-        "currency": "USD",
     },
     {
         "ps_locale": "es-uy",
         "country": "🇺🇾 Uruguay",
-        "currency": "USD",  # UYU
     },
 
-    # Europe, Middle East, Africa, Australia, New Zealand
-    {
-        "ps_locale": "en-au",
-        "country": "🇦🇺 Australia",
-        "currency": "AUD",
-    },
+    # Europe
     {
         "ps_locale": "de-at",
         "country": "🇦🇹 Austria",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "en-bh",
-        "country": "🇧🇭 Bahrain",
-        "currency": "USD",  # BHD
     },
     {
         "ps_locale": "fr-be",
         "country": "🇧🇪 Belgium",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-bg",
         "country": "🇧🇬 Bulgaria",
-        "currency": "EUR",  # BGN
     },
     {
         "ps_locale": "en-hr",
         "country": "🇭🇷 Croatia",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "en-cy",
-        "country": "🇨🇾 Cyprus",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-cz",
         "country": "🇨🇿 Czech Republic",
-        "currency": "CZK",
     },
     {
         "ps_locale": "da-dk",
         "country": "🇩🇰 Denmark",
-        "currency": "DKK",
     },
     {
         "ps_locale": "en-fi",
         "country": "🇫🇮 Finland",
-        "currency": "EUR",
     },
     {
         "ps_locale": "fr-fr",
         "country": "🇫🇷 France",
-        "currency": "EUR",
     },
     {
         "ps_locale": "de-de",
         "country": "🇩🇪 Germany",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-gr",
         "country": "🇬🇷 Greece",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-hu",
         "country": "🇭🇺 Hungary",
-        "currency": "HUF",
     },
     {
         "ps_locale": "en-is",
         "country": "🇮🇸 Iceland",
-        "currency": "EUR",  # ISK
-    },
-    {
-        "ps_locale": "en-in",
-        "country": "🇮🇳 India",
-        "currency": "INR",
     },
     {
         "ps_locale": "en-ie",
         "country": "🇮🇪 Ireland",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "en-il",
-        "country": "🇮🇱 Israel",
-        "currency": "ILS",
     },
     {
         "ps_locale": "it-it",
         "country": "🇮🇹 Italy",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "en-kw",
-        "country": "🇰🇼 Kuwait",
-        "currency": "USD",  # KWD
-    },
-    {
-        "ps_locale": "en-lb",
-        "country": "🇱🇧 Lebanon",
-        "currency": "USD",
     },
     {
         "ps_locale": "de-lu",
         "country": "🇱🇺 Luxembourg",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-mt",
         "country": "🇲🇹 Malta",
-        "currency": "EUR",
     },
     {
         "ps_locale": "nl-nl",
         "country": "🇳🇱 Netherlands",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "en-nz",
-        "country": "🇳🇿 New Zealand",
-        "currency": "NZD",
     },
     {
         "ps_locale": "en-no",
         "country": "🇳🇴 Norway",
-        "currency": "NOK",
-    },
-    {
-        "ps_locale": "ar-om",
-        "country": "🇴🇲 Oman",
-        "currency": "USD",  # OMR
     },
     {
         "ps_locale": "en-pl",
         "country": "🇵🇱 Poland",
-        "currency": "PLN",
     },
     {
         "ps_locale": "pt-pt",
         "country": "🇵🇹 Portugal",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "ar-qa",
-        "country": "🇶🇦 Qatar",
-        "currency": "USD",  # QAR
     },
     {
         "ps_locale": "en-ro",
         "country": "🇷🇴 Romania",
-        "currency": "RON",
     },
-    # {
-    #     "ps_locale": "ru-ru",
-    #     "country": "🇷🇺 Russia",
-    #     "currency": "RUB",
-    # },
     {
-        "ps_locale": "en-sa",
-        "country": "🇸🇦 Saudi Arabia",
-        "currency": "USD",  # SAR
+        "ps_locale": "ru-ru",
+        "country": "🇷🇺 Russia",
     },
-    # {
-    #     "ps_locale": "sr-rs",
-    #     "country": "🇷🇸 Serbia",
-    #     "currency": "EUR",
-    # },
+    {
+        "ps_locale": "sr-rs",
+        "country": "🇷🇸 Serbia",
+    },
     {
         "ps_locale": "en-sk",
         "country": "🇸🇰 Slovakia",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-si",
         "country": "🇸🇮 Slovenia",
-        "currency": "EUR",
-    },
-    {
-        "ps_locale": "en-za",
-        "country": "🇿🇦 South Africa",
-        "currency": "ZAR",
     },
     {
         "ps_locale": "es-es",
         "country": "🇪🇸 Spain",
-        "currency": "EUR",
     },
     {
         "ps_locale": "en-se",
         "country": "🇸🇪 Sweden",
-        "currency": "SEK",
     },
     {
         "ps_locale": "de-ch",
         "country": "🇨🇭 Switzerland",
-        "currency": "CHF",
-    },
-    {
-        "ps_locale": "en-tr",
-        "country": "🇹🇷 Turkey",
-        "currency": "TRY",
     },
     {
         "ps_locale": "uk-ua",
         "country": "🇺🇦 Ukraine",
-        "currency": "UAH",
-    },
-    {
-        "ps_locale": "en-ae",
-        "country": "🇦🇪 United Arab Emirates",
-        "currency": "USD",  # AED
     },
     {
         "ps_locale": "en-gb",
         "country": "🇬🇧 United Kingdom",
-        "currency": "GBP",
+    },
+
+    # Middle East, Africa
+    {
+        "ps_locale": "en-bh",
+        "country": "🇧🇭 Bahrain",
+    },
+    {
+        "ps_locale": "en-il",
+        "country": "🇮🇱 Israel",
+    },
+    {
+        "ps_locale": "en-kw",
+        "country": "🇰🇼 Kuwait",
+    },
+    {
+        "ps_locale": "en-lb",
+        "country": "🇱🇧 Lebanon",
+    },
+    {
+        "ps_locale": "ar-om",
+        "country": "🇴🇲 Oman",
+    },
+    {
+        "ps_locale": "ar-qa",
+        "country": "🇶🇦 Qatar",
+    },
+    {
+        "ps_locale": "en-sa",
+        "country": "🇸🇦 Saudi Arabia",
+    },
+    {
+        "ps_locale": "en-za",
+        "country": "🇿🇦 South Africa",
+    },
+    {
+        "ps_locale": "en-tr",
+        "country": "🇹🇷 Turkey",
+    },
+    {
+        "ps_locale": "en-ae",
+        "country": "🇦🇪 United Arab Emirates",
+    },
+
+    # Australia, New Zealand
+    {
+        "ps_locale": "en-au",
+        "country": "🇦🇺 Australia",
+    },
+    {
+        "ps_locale": "en-nz",
+        "country": "🇳🇿 New Zealand",
     },
 
     # Asia
     {
+        "ps_locale": "en-cy",
+        "country": "🇨🇾 Cyprus",
+    },
+    {
         "ps_locale": "en-hk",
         "country": "🇭🇰 Hong Kong",
-        "currency": "HKD",
+    },
+    {
+        "ps_locale": "en-in",
+        "country": "🇮🇳 India",
     },
     {
         "ps_locale": "en-id",
         "country": "🇮🇩 Indonesia",
-        "currency": "IDR",
     },
     {
         "ps_locale": "ja-jp",
         "country": "🇯🇵 Japan",
-        "currency": "JPY",
     },
     {
         "ps_locale": "zh-hans-cn",
         "country": "🇨🇳 Mainland China",
-        "currency": "CNY",
     },
     {
         "ps_locale": "en-my",
         "country": "🇲🇾 Malaysia",
-        "currency": "MYR",
     },
-    # {
-    #     "ps_locale": "en-ph",
-    #     "country": "🇵🇭 Philippines",
-    #     "currency": "PHP",
-    # },
+    {
+        "ps_locale": "en-ph",
+        "country": "🇵🇭 Philippines",
+    },
     {
         "ps_locale": "en-sg",
         "country": "🇸🇬 Singapore",
-        "currency": "SGD",
     },
     {
         "ps_locale": "ko-kr",
         "country": "🇰🇷 South Korea",
-        "currency": "KRW",
     },
     {
         "ps_locale": "en-tw",
         "country": "🇹🇼 Taiwan",
-        "currency": "TWD",
     },
     {
         "ps_locale": "en-th",
         "country": "🇹🇭 Thailand",
-        "currency": "THB",
     },
-    # {
-    #     "ps_locale": "en-vn",
-    #     "country": "🇻🇳 Vietnam",
-    #     "currency": "USD",
-    # },
+    {
+        "ps_locale": "en-vn",
+        "country": "🇻🇳 Vietnam",
+    },
 ]
