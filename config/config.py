b28df714accec410e18d212557e333f5390afd75
andrey_david <andrey.david.qwerty@gmail.com>
2026-08-16 15:13:06 +0300

    feat: add admin configuration

--- config/config.py
+++ config/config.py
@@ -21,13 +21,21 @@ class LogSettings:
 class Config:
     bot: TgBot
     log: LogSettings
+    admin: Admin
+
+
+@dataclass
+class Admin:
+    admin_ids: list[int]
 
 
 def load_config(path: str | None = None) -> Config:
     env = Env()
     path = '.env'
     env.read_env(path)
+
     return Config(
         bot=TgBot(token=env("BOT_TOKEN")),
         log=LogSettings(level=env("LOG_LEVEL"), format=env("LOG_FORMAT")),
+        admin=Admin(admin_ids=list(map(int, env.list('ADMIN_IDS')))),
     )
