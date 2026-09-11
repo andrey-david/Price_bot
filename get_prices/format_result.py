e6c94b292bae37188eacbfe8b4345a64a9999316
andrey_david <andrey.david.qwerty@gmail.com>
2026-09-11 16:00:41 +0300

    style: align button to the center

--- get_prices/format_result.py
+++ get_prices/format_result.py
@@ -208,7 +208,7 @@ def format_html(
     )
 
     button = f"""
-    <tg-button-row align="right">
+    <tg-button-row align="center">
         <tg-button
             type="switch_inline_query_current_chat"
             style="primary"
